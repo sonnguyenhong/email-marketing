648d50fc66792da04947a21c943da3609625bd6e
sonnguyenhong <sonnguyenhong291@gmail.com>
2023-12-12 23:41:42 +0700

    dashboard: init dashboard

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -14,80 +14,130 @@ import { useTranslation, Trans } from "react-i18next";
 import { trophyImage } from "../assets";
 
 import { ProductsCard } from "../components";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { Line } from "react-chartjs-2";
+
+Chart.register(CategoryScale);
+
+// utils/Data.js
+export const Data = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45677,
+    userLost: 345,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 78888,
+    userLost: 555,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 90000,
+    userLost: 4555,
+  },
+  {
+    id: 5,
+    year: 2020,
+    userGain: 4300,
+    userLost: 234,
+  },
+];
+
+const DATA_COUNT = 7;
+const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
+const labels = ["1", "2", "3", "4", "5", "6", "7"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [45, 54, 77, 63, 65, 88, 92],
+    },
+  ],
+};
+
+const performanceData = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Open rate",
+      data: [11, 9, 12, 12, 14, 16, 20],
+    },
+    {
+      label: "Click rate",
+      data: [4, 8, 9, 10, 11, 8, 9],
+    },
+    {
+      label: "Conversion rate",
+      data: [5, 7, 2, 8, 9, 2, 5],
+    },
+  ],
+};
 
 export default function HomePage() {
   const { t } = useTranslation();
+
   return (
-    <Page narrowWidth>
+    <Page>
       <TitleBar title={t("HomePage.title")} primaryAction={null} />
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Text as="h2" variant="headingMd">
-                    {t("HomePage.heading")}
-                  </Text>
-                  <p>
-                    <Trans
-                      i18nKey="HomePage.yourAppIsReadyToExplore"
-                      components={{
-                        PolarisLink: (
-                          <Link url="https://polaris.shopify.com/" external />
-                        ),
-                        AdminApiLink: (
-                          <Link
-                            url="https://shopify.dev/api/admin-graphql"
-                            external
-                          />
-                        ),
-                        AppBridgeLink: (
-                          <Link
-                            url="https://shopify.dev/apps/tools/app-bridge"
-                            external
-                          />
-                        ),
-                      }}
-                    />
-                  </p>
-                  <p>{t("HomePage.startPopulatingYourApp")}</p>
-                  <p>
-                    <Trans
-                      i18nKey="HomePage.learnMore"
-                      components={{
-                        ShopifyTutorialLink: (
-                          <Link
-                            url="https://shopify.dev/apps/getting-started/add-functionality"
-                            external
-                          />
-                        ),
-                      }}
-                    />
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImage}
-                    alt={t("HomePage.trophyAltText")}
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
+      <div style={{ marginTop: "12px", marginBottom: "24px" }}>
+        <Layout>
+          <Layout.Section>
+            <Card sectioned>
+              <p style={{ fontWeight: 600 }}>Sales from Email Marketing</p>
+              <p>Total revenue this month: 160$</p>
+              <Line
+                data={data}
+                options={{
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: "Sales from Email Marketing",
+                    },
+                    legend: {
+                      display: false,
+                    },
+                  },
+                }}
+              />
+            </Card>
+          </Layout.Section>
+
+          <Layout.Section>
+            <Card sectioned>
+              <p style={{ fontWeight: 600 }}>
+                Email Automation performance this month
+              </p>
+              <Line
+                data={performanceData}
+                options={{
+                  plugins: {
+                    title: {
+                      display: true,
+                      text: "Email Automation performance this month",
+                    },
+                    legend: {
+                      display: true,
+                    },
+                  },
+                }}
+              />
+            </Card>
+          </Layout.Section>
+        </Layout>
+      </div>
     </Page>
   );
 }
